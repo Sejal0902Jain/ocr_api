0b392e22aad390fbd860ba3dc0bcdcd6270da7be
anuragjha <toanuragjha@gmail.com>
2021-04-18 23:21:51 -0700

    ForDocker: Updated for containerizing

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Download and install poppler
 ## Run:
 pip install -r requirements.txt
 
-python apiocrpdf.py
+python app.py
+
+### Docker-compose way
+* To start app, run command `docker-compose up --build`
+* To stop app, run command `Ctrl+C` and then `docker-compose down`
 
 ## API LINK to be used: 
 http://127.0.0.1:5000/convert
--- app.py
+++ app.py
@@ -7,49 +7,51 @@ import os
 
 
 def process(pdf):
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  
+    # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     # path of tesseract.exe 
     PDF_file = pdf
-    pages = convert_from_path(PDF_file, 500,poppler_path=r'C:\Program Files\poppler-0.68.0\bin')  
+    pages = convert_from_path(PDF_file, 500, poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
     # poppler_path 
     # print(pdf)
 
     image_counter = 1
     for page in pages:
-        filename = "page_"+str(image_counter)+".jpg"
+        filename = "page_" + str(image_counter) + ".jpg"
         page.save(filename, 'JPEG')
         image_counter = image_counter + 1
-    
-    filelimit = image_counter-1
+
+    filelimit = image_counter - 1
     outfile = "output.txt"
-    
+
     f = open(outfile, "a")
     for i in range(1, filelimit + 1):
-        filename = "page_"+str(i)+".jpg"
+        filename = "page_" + str(i) + ".jpg"
         text = str(((pytesseract.image_to_string(Image.open(filename)))))
         os.remove(filename)
-        text = text.replace('-\n', '')    
+        text = text.replace('-\n', '')
         f.write(text)
-    
+
     f.close()
     return f
 
 
 PROJECT_HOME = os.path.dirname(os.path.realpath(".\\api_ocrpdf"))
-UPLOAD_FOLDER  = '{}\\api_ocrpdf'.format(PROJECT_HOME)
+UPLOAD_FOLDER = '{}\\api_ocrpdf'.format(PROJECT_HOME)
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/", methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def upload():
     return "OCR_PDF"
 
-@app.route('/convert', methods = ['POST'])  
-def convert():  
+
+@app.route('/convert', methods=['POST'])
+def convert():
     if request.method == 'POST':
         request_data = request.get_json()
-        name= str(request_data['path_PDF'])
+        name = str(request_data['path_PDF'])
 
     if not os.path.exists(name):
         status_code = Response(status=404)
@@ -61,6 +63,7 @@ def convert():
         status_code = Response(status=200)
         return status_code
 
-if __name__ == "__main__":
-    app.debug = False
-    app.run()
+
+# if __name__ == "__main__":
+#     app.debug = False
+#     app.run()
